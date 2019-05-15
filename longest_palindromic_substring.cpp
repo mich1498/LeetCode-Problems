468e5d0a054c292a8248b66aaec82469dd9b14d2
Michael Fernandes <mfernandes2@wisc.edu>
2019-05-14 19:16:43 -0500

    simple DP solution

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -1,3 +1,8 @@
+/*
+* Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+* Input "babad"
+* Output "bab"
+*/
 class Solution {
 public:
     string longestPalindrome(string s) {
