b267bb1c8acc6e599fb384d90b7f87b87e530337
Michael Fernandes <mfernandes2@wisc.edu>
2019-05-22 17:40:49 -0500

    an updated solution , prev O(NlogN ) , now O(m+n)

--- median_of_two_sorted_arrays.cpp
+++ median_of_two_sorted_arrays.cpp
@@ -5,7 +5,7 @@
 * explanation: the median for 1,2,3 is 2.0 
 * NOTE: This is not an optimal solution. Will try to improve on this in the future (Bad Michael :( )
 */
-class Solution {
+/*class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> Median= nums1; 
@@ -25,5 +25,35 @@ public:
             return median/2;
         }
         static const auto speedup = []() { ios::sync_with_stdio( false );    cin.tie( nullptr ); return 0; }();
+    }
+};*/
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size() , n=nums2.size();
+        vector<int> merge;
+        int i=0, j=0 ;
+        while (i<m && j<n){
+            if (nums1[i] < nums2[j]){
+                merge.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                merge.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while (i<m) merge.push_back(nums1[i++]);
+        while (j<n) merge.push_back(nums2[j++]);
+
+        if ((m+n)%2==1){
+           int const mid= (m+n)/2 ;
+            return merge[mid];
+        }
+        else {
+            int const mid = (m+n)/2;
+            double median= merge[mid-1] + merge[mid];
+            return median/2;
+        }
     }
 };
